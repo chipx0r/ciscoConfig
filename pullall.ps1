ece72d960ab095eebb41df9313252e827bc4c673
chipx0r <chipx0r@gmail.com>
2019-04-03 00:44:06 -0600

    Create pullall.ps1

---
+++ pullall.ps1
@@ -0,0 +1,15 @@
+Cls
+Write-Host "Searching for repositories to update..."
+Write-Host
+
+$dirs = Get-ChildItem | where {$_.PsIsContainer} | where { Get-ChildItem $_ -filter ".hg" } 
+
+Write-Host "Updating " @($dirs).length
+Write-Host 
+
+foreach ($dir in $dirs)
+{
+		cd $dir.FullName
+		hg pull
+		cd ../
+}
