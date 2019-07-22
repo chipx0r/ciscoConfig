24d9a3b76ffb8fd1809f1a2010fe12e603d90d70
chipx0r <chipx0r@gmail.com>
2019-07-22 17:21:42 -0500

    Update countFiles.sh

--- countFiles.sh
+++ countFiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # ls | grep ^....18\- | xargs -I '{}' mv -v '{}' 2018/
+# find . -newermt 20180101 -not -newermt 20190101 -maxdepth 1 -type f -name '*.pdf' -exec mv -v {} 2018/ \;
 #
 find . ! -path . -maxdepth 1 -type d -print0 | while IFS= read -r -d '' i ; do
     echo -n $i": " ; 
