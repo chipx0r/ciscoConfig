28880febbb2c1bc9b6b05139528df3317feff160
chipx0r <chipx0r@gmail.com>
2024-08-17 16:20:17 -0600

    JenkinsTrigger

---
+++ jenkins.py
@@ -0,0 +1,33 @@
+import urllib, urllib2, base64
+targetbranch = 'UAT'
+username = 'admin'
+password = 'tokenpass'
+jenkinstoken = 'uat'
+
+def doCall():
+    data = {'url': '@REPO_URL@',
+       'branch': repo[node].branch(),
+       'changesetId': node}
+
+    base64string = base64.b64encode('%s:%s' % (username, password))
+    url = 'http://192.168.4.16:8080/job/AspNet8DockerBuild/build?token=' + jenkinstoken
+    print 'CI-CD trigger:' + url
+    req = urllib2.Request(url)
+    req.add_header('Authorization', 'Basic %s' % base64string)
+    rsp = urllib2.urlopen(req, timeout=10)
+
+
+def calljenkins(ui, repo, node, **kwargs):
+    send = False
+    for rev in xrange(repo[node].rev(), len(repo)):
+        ctx = repo[rev]
+        if ctx.branch() == targetbranch:
+            send = True
+
+    if send:
+        doCall()
+    else:
+        print 'No CI-CD trigger'
+
+    pass
+
